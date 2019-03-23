c2bf0e931a4914cbae5ef51454ad84e21750083b
Sod Yat <sodamyat31@gmail.com>
2019-03-23 08:46:17 +0300

    first file

---
+++ disc.pas
@@ -0,0 +1,18 @@
+program disc;
+
+uses SysUtils;
+var A,B,C,D,E,x1,x2:single;
+Begin
+WriteLn('input a,b,c:');
+ReadLn(A,B,C);
+D:= Sqr(B) - (4*A*C);
+if D>=0 then
+begin
+E:=2*A;
+X1:=(-B+Sqrt(D))/E;
+X2:=(-B-Sqrt(D))/E;
+WriteLn('x1',x1,'x2',x2)
+end
+else WriteLn('no result');
+ReadLn;
+end.
